e6b404067ec2632c53a9eedfaecf771822d2b6ed
O'Neil Delpratt <oneil@saxonica.com>
2020-10-28 14:44:42 +0000

    Saxon 10.3 - update to the build number

--- latest10/hej/net/sf/saxon/Version.java
+++ latest10/hej/net/sf/saxon/Version.java
@@ -15,7 +15,7 @@ public final class Version {
 
     private static final int MAJOR_VERSION = 10;
     private static final int MINOR_VERSION = 3;
-    private static final int BUILD = 102813; //mddhh
+    private static final int BUILD = 102815; //mddhh
     private static final String MAJOR_RELEASE_DATE = "2020-03-16";
     private static final String MINOR_RELEASE_DATE = "2020-10-28";
 
